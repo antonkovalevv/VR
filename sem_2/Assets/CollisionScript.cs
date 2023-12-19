187c502c37b4b20453f8b4c989a6c97d52e5640c
Kovalev Anton <anton.kovalev2005@mail.ru>
2023-12-20 02:07:00 +0300

    zadanie_2

---
+++ sem_2/Assets/CollisionScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionScript : MonoBehaviour
+{
+    public int score = 100;
+    
+    public void OnCollisionEnter()
+    {
+        Debug.Log("working");
+        score = score - 1;
+    }
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
